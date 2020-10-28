742df6f463cca7f68e5e45536d6f3c35c31c9103
oraculocs <augustino2003@hotmail.com>
2020-10-28 19:24:03 -0300

    Atualização do service na API

--- utils/utils.js
+++ utils/utils.js
@@ -3,53 +3,32 @@ const db = require('../db/connection')
 
 module.exports = {
     salvarRegistro: (req, res, next) => {
-        let post = req.body
-
-        let titulo = post.titulo;
-        let descricao = post.descricao;
-        let imagem = post.imagem;
-
+        let post = req.body;
         let nome = post.nome;
         let cpf = post.cpf;
         let numero = post.numero;
         let email = post.email; 
         let termo = post.termo;
 
-      
-            if (err) {
-                res.sendStatus(403);
-            } else {
-                let query = `insert into numerosClonados (nome,cpf,numero,email,termo) values ('${nome}','${cpf}','${numero}','${email}','${termo}')`;
-                db.execSQLQuery().query(query, function (error, results, fields) {
-                    if (error)
-                        res.json(error);
-                    else
-                        res.json(results);
+        let query = `insert into numerosClonados (nome,cpf,numero,email,termo) values ('${nome}','${cpf}','${numero}','${email}','${termo}')`;
+        db.execSQLQuery().query(query, function (error, results, fields) {
+            if (error)
+                res.json(error);
+            else
+                res.json(results);
+        });
+        db.execSQLQuery().end();
 
-                });
-                db.execSQLQuery().end();
-            }
     },
     verificarNumero: (req, res, next) => {
-
-            if (err) {
-                res.sendStatus(403);
-            } else {
-                // let query = "select * from numerosClonados";
-                let query = `select * from numerosClonados where numero = '${numero}'`;
-                db.execSQLQuery().query(query, function (error, results, fields) {
-                    if (error)
-                        res.json(error);
-                    else {
-
-                        res.json(results);
-                    }
-                    db.execSQLQuery().end();
-                });
-
+        let query = `select * from numerosClonados where numero = '${numero}'`;
+        db.execSQLQuery().query(query, function (error, results, fields) {
+            if (error)
+                res.json(error);
+            else {
+                res.json(results);
             }
-
-
-        console.log("VALOR DE RES: ", res)
+            db.execSQLQuery().end();
+        });
     },
 }
